587f276fa6d65aac3c8f797dcad02206fa988fa3
Kalam Mahardhika <kalammahardhika1002@gmail.com>
2022-11-29 18:01:09 +0700

    register error fix

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{ $title }}</title>
+
+  @vite('resources/css/app.css')
+</head>
+<body class="h-full">
+
+  <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+    <svg
+      class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+      viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3"
+        d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
+      <defs>
+        <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645"
+          gradientUnits="userSpaceOnUse">
+          <stop stop-color="#9089FC"></stop>
+          <stop offset="1" stop-color="#FF80B5"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
+
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img class="mx-auto h-16 w-auto" src="./img/logo.png"
+          alt="Your Company">
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register account</h2>
+      </div>
+
+      <form class="mt-8 space-y-6" action="/register" method="POST">
+        @csrf
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="name" class="sr-only">Name</label>
+            <input id="name" name="name" type="text" autocomplete="name" required
+              class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm @error('name') is-invalid @enderror"
+              placeholder="Name">
+              
+              @error('name')
+                  <p class="invalid-feedback ml-1 my-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+              @enderror
+          </div>
+
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required
+              class="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm @error('email') is-invalid @enderror"
+              placeholder="Email address">
+
+              @error('email')
+                  <p class="invalid-feedback ml-1 my-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+              @enderror
+          </div>
+
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="password" required
+              class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm @error('password') is-invalid @enderror"
+              placeholder="Password">
+
+              @error('password')
+                  <p class="invalid-feedback ml-1 my-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+              @enderror
+          </div>
+        </div>
+
+        <div>
+          <button type="submit"
+            class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <!-- Heroicon name: mini/lock-closed -->
+              <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd"
+                  d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                  clip-rule="evenodd" />
+              </svg>
+            </span>
+            Register
+          </button>
+
+          <p class="text-sm text-gray-500 text-center mt-4">Login if you have an <a href="/login"
+              class="text-indigo-600 hover:text-indigo-500">account</a></p>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div
+    class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+    <svg
+      class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+      viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fill-opacity=".3"
+        d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
+      <defs>
+        <linearGradient id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc" x1="1155.49" x2="-78.208" y1=".177" y2="474.645"
+          gradientUnits="userSpaceOnUse">
+          <stop stop-color="#9089FC"></stop>
+          <stop offset="1" stop-color="#FF80B5"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
+
+</body>
+</html>
