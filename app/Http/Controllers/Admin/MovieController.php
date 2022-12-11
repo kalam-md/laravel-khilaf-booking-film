42a4aeb631b9c0ad815238a219f537957648426f
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2022-12-11 17:40:20 +0700

    Membuat halaman movie admin

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Movie;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,9 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard-admin.movie.index', [
+            'movies' => Movie::all()
+        ]);
     }
 
     /**
