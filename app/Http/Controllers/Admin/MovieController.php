7c4b3d57d21ab6d57bab45ba9eb6d00821543577
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2022-12-11 17:52:02 +0700

    Create Data Movie Admin

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Movie;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class MovieController extends Controller
 {
@@ -27,7 +28,9 @@ class MovieController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard-admin.movie.create', [
+            'categories' => Category::all()
+        ]);
     }
 
     /**
