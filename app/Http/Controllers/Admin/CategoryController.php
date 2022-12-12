b91d202fb2d33461a99d14da4c520661b121763c
rafinurilakbarf <203040135@mail.unpas.ac.id>
2022-12-12 11:10:37 +0700

    delete data category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -102,7 +102,7 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
         
         Category::destroy($category->id);
